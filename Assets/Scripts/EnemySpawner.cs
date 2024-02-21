1bf02b153244a26012af9ed92436041985a29293
Alexey <84139032+VaTTTT@users.noreply.github.com>
2024-02-21 13:35:22 +0300

    Update

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,6 +6,8 @@ using UnityEngine.UIElements;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private float _pauseTime;
+    [SerializeField] private bool _isOrderRandom;
+    [SerializeField] private int _quantityOfEnemies;
 
     private EnemySpawnPoint[] _spawnPoints;
     private Vector2 _spawnLocation;
@@ -23,13 +25,44 @@ public class EnemySpawner : MonoBehaviour
 
     private IEnumerator SpawnEnemies(float pauseTime)
     {
-        for (int i = 0; i < _spawnPoints.Length; i++)
-        {
-            _spawnLocation = new Vector2(_spawnPoints[i].transform.position.x, _spawnPoints[i].transform.position.y);
-            Enemy newEnemy = Instantiate(_spawnPoints[i].GetEnemy(), _spawnLocation, Quaternion.identity, _spawnPoints[i].transform);
-            newEnemy.SetMovementSpeed(_spawnPoints[i].GetEnemyMovementSpeed());
+        int enemyCounter = 0;
+        int spawnPointIndex;
 
-            yield return new WaitForSeconds(pauseTime);
+        if (!_isOrderRandom)
+        {
+            spawnPointIndex = 0;
+
+            while (enemyCounter < _quantityOfEnemies)
+            {
+                _spawnLocation = new Vector2(_spawnPoints[spawnPointIndex].transform.position.x, _spawnPoints[spawnPointIndex].transform.position.y);
+                Enemy newEnemy = Instantiate(_spawnPoints[spawnPointIndex].GetEnemy(), _spawnLocation, Quaternion.identity, _spawnPoints[spawnPointIndex].transform);
+
+                newEnemy.SetMovementSpeed(_spawnPoints[spawnPointIndex].GetEnemyMovementSpeed());
+                enemyCounter++;
+                spawnPointIndex++;
+
+                if (spawnPointIndex >= _spawnPoints.Length)
+                {
+                    spawnPointIndex = 0;
+                }
+
+                yield return new WaitForSeconds(pauseTime);
+            }
+        }
+        else
+        {
+            while (enemyCounter < _quantityOfEnemies)
+            {
+                spawnPointIndex = Random.Range(0, _spawnPoints.Length);
+
+                _spawnLocation = new Vector2(_spawnPoints[spawnPointIndex].transform.position.x, _spawnPoints[spawnPointIndex].transform.position.y);
+                Enemy newEnemy = Instantiate(_spawnPoints[spawnPointIndex].GetEnemy(), _spawnLocation, Quaternion.identity, _spawnPoints[spawnPointIndex].transform);
+
+                newEnemy.SetMovementSpeed(_spawnPoints[spawnPointIndex].GetEnemyMovementSpeed());
+                enemyCounter++;
+
+                yield return new WaitForSeconds(pauseTime);
+            }
         }
     }
 }
